ff1adff53007a054e9ea26e8a77cbdb0bfb93ae1
Amber - Home <114544163+Amber5kye@users.noreply.github.com>
2023-08-13 16:29:45 +1000

    Sound Manager Script Update

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -2,98 +2,123 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class SoundManager : MonoBehaviour
 {
-    //public AudioSource playerSounds;
-    //public AudioSource gunSounds;
-    //public AudioSource zombieSounds;
-    //public AudioSource backgroundMusic;
+    public AudioSource playerSounds;
+    public AudioSource gunSounds;
+    public AudioSource zombieSounds;
+    public AudioSource backgroundMusic;
 
-    //public AudioClip[] playerWalkingSounds;
-    //public AudioClip[] playerRunningSounds;
-    //public AudioClip playerDeathSound; //played upon players health reaching 0.
+    public AudioClip[] playerWalkingSounds;
+    public AudioClip[] playerRunningSounds;
+    public AudioClip playerDeathSound; //played upon players health reaching 0.
 
-    //public AudioClip gunShotSound; 
-    //public AudioClip gunReloadSound; //played after gunshot sound.
+    public AudioClip gunShotSound;
+    public AudioClip gunReloadSound; //played after gunshot sound.
 
-    //public AudioClip[] zombieAmbienceSounds; //starts at radius 13, increase in volume till loudest at 1.
-    //public AudioClip zombieDeathSound; //upon zombie health reaching 0. 
-    //public AudioClip zombieAlertSound; //starts at radius 3.
+    public AudioClip[] zombieAmbienceSounds; //starts at radius 13, increase in volume till loudest at 1.
+    public AudioClip zombieDeathSound; //upon zombie health reaching 0. 
+    public AudioClip zombieAlertSound; //starts at radius 3.
 
-    //public AudioClip menuMusic; //played after a brief delay on menu screen.
-    //public AudioClip gameMusic; //played after a brief delay upon entering map/scene.
-    //public AudioClip loadSound; //played on teleport loads. (If included).
+    public AudioClip menuMusic; //played after a brief delay on menu screen.
+    public AudioClip gameMusic; //played after a brief delay upon entering map/scene.
+    public AudioClip loadSound; //played on teleport loads. (If included).
 
-    //private void Awake()
-    //{
-    //    if (instance == null)
-    //    {
-    //        instance = this;
-    //        DontDestroyOnLoad(gameObject);
-    //    }
-    //    else
-    //    {
-    //        Destroy(gameObject);
-    //    }
-    //}
+    private Transform _controller;
 
-    //void Start()
-    //{
-    //    StartCoroutine(PlayDelayedMenuMusic());
-    //    StartCoroutine(PlayDelayedGameMusic());
-    //}
+    void Start()
+    {
+        StartCoroutine(PlayDelayedGameMusic());
+    }
 
-    //IEnumerator PlayDelayedGameMusic()
-    //{
-    //    yield return new WaitForSeconds(0.1f);
-    //    PlayGameMusic();
-    //}
+    IEnumerator PlayDelayedGameMusic()
+    {
+        yield return new WaitForSeconds(0.1f);
+        PlayGameMusic();
+    }
 
-    //public void PlayEnemyAmbienceSound(Vector3 enemyPosition)
-    //{
-    //    float distanceToEnemy = Vector3.Distance(enemyPosition, playerTransform.position);
-    //    float maxAudibleDistance = 13f;
-    //    float volumeFactor = 1f - Mathf.Clamp01(distanceToEnemy / maxAudibleDistance);
+    public void PlayGameMusic()
+    {
+        backgroundMusic.clip = gameMusic;
+        backgroundMusic.Play();
+    }
 
-    //    if (enemyAmbienceSounds.Length > 0)
-    //    {
-    //        AudioClip clip = enemyAmbienceSounds[Random.Range(0, enemyAmbienceSounds.Length)];
-    //        enemySoundsAudioSource.PlayOneShot(clip, volumeFactor);
-    //    }
-    //}
 
-    // public void PlayEnemyAttack(Vector3 enemyPosition)
-    //{
-    //    float distanceToEnemy = Vector3.Distance(enemyPosition, playerTransform.position);
-    //    float maxAudibleDistance = 3f;
-    //    float volumeFactor = 1f - Mathf.Clamp01(distanceToEnemy / maxAudibleDistance);
+    public void PlayZombieAmbienceSound(Vector3 enemyPosition)
+    {
+        float distanceToEnemy = Vector3.Distance(enemyPosition, _controller.position);
+        float maxAudibleDistance = 13f;
+        float volumeFactor = 1f - Mathf.Clamp01(distanceToEnemy / maxAudibleDistance);
 
-    //    if (enemyAttackSound != null)
-    //    {
-    //        enemySoundsAudioSource.PlayOneShot(enemyAttackSound, volumeFactor);
-    //    }
-    //}
+        if (zombieAmbienceSounds.Length > 0)
+        {
+            AudioClip clip = zombieAmbienceSounds[Random.Range(0, zombieAmbienceSounds.Length)];
+            zombieSounds.PlayOneShot(clip, volumeFactor);
+        }
+    }
 
-    //public void HandlePlayerMovement(bool isRunning)
-    //{
-    //    if (isRunning)
-    //    {
-    //        PlayRandomPlayerRunningSound();
-    //    }
-    //    else
-    //    {
-    //        PlayRandomPlayerWalkingSound();
-    //    }
-    //}
+    public void PlayZombieAlert(Vector3 enemyPosition)
+    {
+        float distanceToEnemy = Vector3.Distance(enemyPosition, _controller.position);
+        float maxAudibleDistance = 3f;
+        float volumeFactor = 1f - Mathf.Clamp01(distanceToEnemy / maxAudibleDistance);
+
+        if (zombieAlertSound != null)
+        {
+            zombieSounds.PlayOneShot(zombieAlertSound, volumeFactor);
+        }
+    }
+
+    public void HandlePlayerMovement(bool isRunning)
+    {
+        if (isRunning)
+        {
+            PlayPlayerRunningSounds();
+        }
+        else
+        {
+            PlayPlayerWalkingSounds();
+        }
+    }
+
+    public void PlayPlayerRunningSounds()
+    {
+        if (playerRunningSounds.Length > 0)
+        {
+            AudioClip clip = playerRunningSounds[Random.Range(0, playerRunningSounds.Length)];
+            playerSounds.PlayOneShot(clip);
+        }
+    }
+
+    public void PlayPlayerWalkingSounds()
+    {
+        if (playerWalkingSounds.Length > 0)
+        {
+            AudioClip clip = playerWalkingSounds[Random.Range(0, playerWalkingSounds.Length)];
+            playerSounds.PlayOneShot(clip);
+        }
+    }
 
     //public void HandlePlayerDeath()
     //{
     //    PlayPlayerDeathSound();
     //}
 
-    //public void HandleGunShot()
-    //{
-    //    PlayGunShotSound();
-    //    PlayGunReloadSound(); 
-    //}
+    public void HandleGunShot()
+    {
+        PlayGunShotSound(); 
+        PlayGunReloadSound(); 
+    }
+
+    private void PlayGunShotSound()
+    {
+        gunSounds.PlayOneShot(gunShotSound);
+    }
+
+    private void PlayGunReloadSound()
+    {
+        gunSounds.PlayOneShot(gunReloadSound);
+    }
 }
+
